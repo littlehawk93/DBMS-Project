aa4a30f68f4a113fe9cbd142d6264f2fc3551616
John <littlehawk@ubuntu.(none)>
2014-01-15 00:01:44 -0600

    added updater file

--- codeupload.php
+++ codeupload.php
@@ -1,7 +1,7 @@
 <?php
  
 if ( $_POST['payload'] ) {
-  shell_exec( 'git reset --hard HEAD && git pull https://github.com/littlehawk93/DBMS-Project.git' );
+  shell_exec( './update.sh' );
 }else {
 	header("Location: index.html");
 }
---
+++ update.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+/usr/lib/git-core/git reset --hard HEAD && /usr/lib/git-core/git pull https://github.com/littlehawk93/DBMS-Project.git;
+
+rm -f LICENSE;
+rm -f README.md;
