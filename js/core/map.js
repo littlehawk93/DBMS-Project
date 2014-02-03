38718c7765789fdf541c77101278abd216b74336
James Miltenberger <jamilt13@gmail.com>
2014-02-03 07:58:33 -0600

    Added new USA map. added an image to the main page

--- js/core/map.js
+++ js/core/map.js
@@ -2,3 +2,12 @@
 *maps.js handles the canvas that holds the map of the USA
 */
 
+$(document).ready(function() {
+	/*var canvas = document.createElement('canvas');
+	canvas.id = "canvas";
+	canvas.width = 800;
+	canvas.height = 300;
+	canvas.style.backgroundImage = ("url(img/usa.png)");
+	canvas.style.border = "ridge";
+	$("#map_div").append(canvas);*/
+});
