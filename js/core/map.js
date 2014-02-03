9831874ceb01968073ef17912f0ad0bf25d81426
James Miltenberger <jamilt13@gmail.com>
2014-02-03 14:20:55 -0600

    Added ability to click some of the states. Added animations for switching maps

--- js/core/map.js
+++ js/core/map.js
@@ -3,11 +3,18 @@
 */
 
 $(document).ready(function() {
-	/*var canvas = document.createElement('canvas');
-	canvas.id = "canvas";
-	canvas.width = 800;
-	canvas.height = 300;
-	canvas.style.backgroundImage = ("url(img/usa.png)");
-	canvas.style.border = "ridge";
-	$("#map_div").append(canvas);*/
+	$("area").click(function() {
+		var clicked = $(this);
+		$("#usa").hide(500);
+		//alert("You clicked on: " + $(this).attr("title"));
+		$("#map_div").append("<button class='state' onclick='goBack()' class='state'>Back</button><br class='state'>" + 
+			"<img class='state' src='img/state/" + clicked.attr("title") + ".gif'></img>");
+		return false;
+	});
 });
+
+function goBack() {
+	$("#map_div").children(".state").hide(500, function() { $(this).remove(); });
+	$("#usa").show(500);
+	return false;
+}
