8fad4f403a1adfac73b4a89991a2a6d04238a1af
James Miltenberger <jamilt13@gmail.com>
2014-02-03 22:43:38 -0600

    Changed button appearance and position.

--- js/core/map.js
+++ js/core/map.js
@@ -3,18 +3,20 @@
 */
 
 $(document).ready(function() {
+	$("#back_button").hide();
 	$("area").click(function() {
 		var clicked = $(this);
 		$("#usa").hide(500);
 		//alert("You clicked on: " + $(this).attr("title"));
-		$("#map_div").append("<button class='state' onclick='goBack()' class='state'>Back</button><br class='state'>" + 
-			"<img class='state' src='img/state/" + clicked.attr("alt") + ".gif'></img>");
+		$("#back_button").show(500);
+		$("#map_div").append("<br class='state'><img class='state' src='img/state/" + clicked.attr("alt") + ".gif'></img>");
 		return false;
 	});
 });
 
 function goBack() {
-	$("#map_div").children(".state").hide(500, function() { $(this).remove(); });
+	$("#map_div").children(".state, #button").hide(500, function() { $(this).remove(); });
+	$("#back_button").hide(500);
 	$("#usa").show(500);
 	return false;
 }
